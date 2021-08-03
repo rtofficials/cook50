8c86aceaf7d6376ee19a0bc65a5c6f802e411a0a
rtofficials <rtofficials@gmail.com>
2021-08-03 16:30:46 +0530

    added another line of insertion

--- post.sh
+++ post.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-curl "http://127.0.0.1:5000/add_recipe" \
--d "rname=Aaloo Tikki&rating=2&difficulty_level=5&type=Evening Snack&prep_time=00:45:00&main_ingredients=aaloo, oil, curd&last_made=2021-04-10&details_link=https://somerecipesite/aaloo_tikki_for_begineers"
+curl "http://localhost:5000/add_recipe" -d "rname=Chicken Parmesian&rating=6&difficulty_level=5&type=Dinner&prep_time=00:30:00&main_ingredients=chiken, salt, spices&last_made=2021-07-22&details_link=https://www.somerecipesite.com/chiken parmesian";
+
+curl "http://localhost:5000/add_recipe" -d "rname=Kadhai Paneer&rating=9&difficulty_level=8&type=Dinner&prep_time=00:40:00&main_ingredients=Paneer, Kadhai, salt, water, butter,Cashew Nuts&last_made=2021-05-18&details_link=https://www.somerecipesite.com/kadhai-paneer"; 
