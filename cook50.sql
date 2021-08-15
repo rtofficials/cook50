97617ae45d7c863bb648d4c8ed10f5afd8017d96
iwwar <zippetizap@gmail.com>
2021-08-15 19:37:18 +0530

    database structured

--- cook50.sql
+++ cook50.sql
@@ -1,8 +1,8 @@
 CREATE TABLE recipes(
        'rname' TEXT NOT NULL UNIQUE,
-       'rating' INTEGER NOT NULL,
+       'rating' INTEGER NOT NULL  CHECK (rating<6),
        'difficulty_level' TEXT NOT NULL,
-       'type' TEXT NOT NULL,
+       'r_type' TEXT NOT NULL,
        'prep_time' TIME NOT NULL,
        'main_ingredients' TEXT NOT NULL,
        'last_made' DATE,
