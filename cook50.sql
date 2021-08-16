64c3ed209e26e8ac798ac3e1d3aed586597b2e34
rtofficals <rtofficals@gmail.com>
2021-08-16 22:08:44 +0530

    database updated

--- cook50.sql
+++ cook50.sql
@@ -1,7 +1,7 @@
 CREATE TABLE recipes(
        'rname' TEXT NOT NULL UNIQUE,
        'rating' INTEGER NOT NULL  CHECK (rating<6),
-       'difficulty_level' TEXT NOT NULL,
+       'difficulty_level' INTEGER NOT NULL,
        'r_type' TEXT NOT NULL,
        'prep_time' TIME NOT NULL,
        'main_ingredients' TEXT NOT NULL,
