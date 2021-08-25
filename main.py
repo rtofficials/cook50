2233fad697b71bf68c6246f5680ca1a2a4258eea
rtofficals <rtofficals@gmail.com>
2021-08-25 17:03:04 +0530

    meal-planner view added

--- main.py
+++ main.py
@@ -22,14 +22,14 @@ def add_recipe():
         main_ingredients = request.form["main_ingredients"]
         last_made =  request.form["last_made"]
         details_link = request.form["details_link"]
-        params = (rname, rating, difficulty_level, r_type, prep_time, main_ingredients, last_made, details_link)
+        recipe_data = (rname, rating, difficulty_level, r_type, prep_time, main_ingredients, last_made, details_link)
         print(params)
         con = sqlite3.connect("cook50.db")
         print("add : established")
         c = con.cursor();
         print("add : cursor created")
         try:
-            c.execute("INSERT INTO recipes VALUES(?, ?, ?, ?, ?, ?, ?, ?)", params)
+            c.execute("INSERT INTO recipes VALUES(?, ?, ?, ?, ?, ?, ?, ?)", recipe_data)
             con.commit()
             con.close()
             return "Recipe Added.\n"
@@ -48,15 +48,30 @@ def recipe_list():
     rows = c.fetchall()
     return render_template("recipe_list.html",  rows=rows)
 
-@app.route("/meal-planner")
+@app.route("/meal_planner", methods=["POST", "GET"])
 def meal_planner():
     con = sqlite3.connect("cook50.db")
     print("planner : established")
     c = con.cursor()
     print("planner : cursor created")
-    c.execute("SELECT rowid, day, date, mealtype, mealname FROM MEALS;")
+    try :
+        if request.method == "POST":
+            day = request.form["day"]
+            date = request.form["date"]
+            meal_type = request.form["mealtype"]
+            meal_name = request.form["mealname"]
+            meal = (day, date, meal_type, meal_name)
+            print(meal)
+            c.execute("INSERT INTO MEALS VALUES (?, ?, ?, ?)", meal)
+            print("inserted")
+            con.commit()
+            return "meal planned."
+    c.execute("SELECT rowid, *  FROM MEALS;")
     meals = c.fetchall();
+    con.close()
     return render_template("meal_planner.html", meals=meals)
+
+
 
 @app.route("/timer")
 def timer():
